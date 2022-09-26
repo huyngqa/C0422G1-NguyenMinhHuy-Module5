2bd059823a01cef4242d780161ba54087bc9feb4
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-09-26 16:10:43 +0700

    ss5

---
+++ 05-components-interaction/components-interaction/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NameCardComponent } from './practice/name-card/name-card.component';
+import { ProgressBarComponent } from './practice/progress-bar/progress-bar.component';
+import { RatingBarComponent } from './exercise/rating-bar/rating-bar.component';
+import { CountdownTimerComponent } from './exercise/countdown-timer/countdown-timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NameCardComponent,
+    ProgressBarComponent,
+    RatingBarComponent,
+    CountdownTimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
